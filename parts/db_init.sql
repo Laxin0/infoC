7dc3dd06bdf825ad9d886bdb151ffead30652096
LAXIN <laxin530@gmail.com>
2025-04-07 17:07:20 +0300

    added sql db init file

---
+++ parts/db_init.sql
@@ -0,0 +1,18 @@
+CREATE DATABASE infocenter_db;
+
+USE infocenter_db;
+
+CREATE TABLE pages(
+    id int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    parent_id int,
+    path varchar(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO pages (name, parent_id, path)
+VALUES ('Корень', NULL, "root.htm"),       -- 1
+        ('Общежитие', 1, 'dormitory.htm'), -- 2
+        ('Направления', 1, 'specs.htm'),   -- 3
+        ('Строительство', 3, 'build.htm'), -- 4
+        ('Информатика', 3, 'it.htm');      -- 5
