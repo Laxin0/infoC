6510d7bb332ae5736e5789b3ca30e51d423ee828
laxin <laxin530@gmail.com>
2025-04-10 13:03:31 +0300

    add .env support

--- db_connection.php
+++ db_connection.php
@@ -1,9 +1,32 @@
 <?php
 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "infocenter_db";
+function loadEnv($path)
+{
+    if (!file_exists($path)) return;
+
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) continue;
+
+        list($key, $value) = explode('=', $line, 2);
+        $key = trim($key);
+        $value = trim($value);
+
+        putenv("$key=$value");
+        $_ENV[$key] = $value;
+        $_SERVER[$key] = $value;
+    }
+}
+
+loadEnv(__DIR__ . '/.env.local');
+$dbhost = getenv("DB_HOST");
+$dbuser = getenv("DB_USER");
+$dbpass = getenv("DB_PASS");
+$dbname = getenv("DB_NAME");
+
+#echo $dbhost . "/" . $dbuser . "/" . $dbpass . "/" . $dbname . "<br>";
+
 $mysql = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
 
 if ($mysql->connect_error) {
