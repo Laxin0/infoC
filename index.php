eb7c61998b439aaed4136a238734c237b9d7525e
LAXIN <laxin530@gmail.com>
2025-04-15 18:55:18 +0300

    added little style

--- index.php
+++ index.php
@@ -6,10 +6,18 @@
     <title>InfoC</title>
     <h1 id="name"></h1>
     <script type="text/javascript" src="script.js"></script>
+    <style>
+    button{
+        border: none;
+        padding: 10px;
+        background-color: #F1ECCE;
+        border-radius: 10px;
+    }
+    </style>
 </head>
 <body>
-    <button onclick="goBack()">Back</button><br>
-    <p id="path" style="background-color: antiquewhite;"></p>
+    <button onclick="goBack()">Назад</button><br>
+    <hr>
     <div id="content"></div>
     <hr>
     <div id="child_nodes">
