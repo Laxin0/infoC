07adc170731290d4a52a83d2d4c3cedb8d5bb151
laxin <laxin530@gmail.com>
2025-05-07 09:49:19 +0300

    changed ! to NOT

--- toggleCallStatus.php
+++ toggleCallStatus.php
@@ -7,7 +7,7 @@ $data = json_decode($inputJSON, true);
 
 $id = $data['id'];
 
-$result = $mysql->query("UPDATE calls SET is_solved=!is_solved WHERE id=$id");
+$result = $mysql->query("UPDATE calls SET is_solved= NOT is_solved WHERE id=$id");
 
 echo(json_encode(["status" => $result ? "ok" : "err"]));
 
